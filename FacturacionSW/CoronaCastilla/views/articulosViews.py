0d66d21c52394dcf2e2c5be8f508014b4edda157
mlevitorresv <levitorres66@gmail.com>
2024-07-09 12:44:51 +0200

    showing articles info

--- FacturacionSW/CoronaCastilla/views/articulosViews.py
+++ FacturacionSW/CoronaCastilla/views/articulosViews.py
@@ -3,4 +3,4 @@ from CoronaCastilla.models import Articulo
 
 def view_articulos(request):
     articulos = Articulo.objects.all()
-    return render(request, 'articulos.html', {'articulo' : Articulo})
+    return render(request, 'articulos.html', {'articulos' : articulos})
