5b420237e625f6a432a45f86e4888339770bb374
mlevitorresv <levitorres66@gmail.com>
2024-07-09 12:17:37 +0200

    view_facturasstres query completed

--- FacturacionSW/CoronaCastilla/views/facturasViews.py
+++ FacturacionSW/CoronaCastilla/views/facturasViews.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from CoronaCastilla.models import Factura
 from datetime import datetime
 
@@ -19,7 +20,21 @@ def view_facturas_tres(request):
     mes_actual = ahora.month
     año_actual = ahora.year
     
-    facturas = Factura.objects.filter(fecha_salida__year =año_actual, fecha_salida__month=mes_actual, fecha_salida__month=mes_actual - 1, fecha_salida__month=mes_actual - 2)
+    meses = [mes_actual, (mes_actual - 1) % 12 or 12, (mes_actual - 2) % 12 or 12]
+    años = [año_actual] * 3
+    
+    if mes_actual == 1:
+        años[1] -= 1
+        años[2] -= 1
+    elif mes_actual == 2:
+        años[2] -= 1
+
+    query = Q(fecha_salida__year=años[0], fecha_salida__month=meses[0]) | \
+            Q(fecha_salida__year=años[1], fecha_salida__month=meses[1]) | \
+            Q(fecha_salida__year=años[2], fecha_salida__month=meses[2])
+    
+    
+    facturas = Factura.objects.filter(query)
     return render(request, 'facturas.html', {'facturas' : facturas})
 
 
