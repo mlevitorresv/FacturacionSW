b4288021eb16aa2265c00814a6f9a7d9bcc92323
mlevitorresv <levitorres66@gmail.com>
2024-07-11 12:09:36 +0200

    m for articulos changed, added column categoria

--- FacturacionSW/CoronaCastilla/models.py
+++ FacturacionSW/CoronaCastilla/models.py
@@ -1,13 +1,22 @@
 from django.db import models
-
-# Create your models here.
 class Articulo(models.Model):
+    CATEGORIAS = [
+        ('alojamiento', 'Alojamiento'),
+        ('desayuno', 'Desayuno'),
+    ]
+
     nombre = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=20, choices=CATEGORIAS)
     precio1 = models.FloatField()
     precio2 = models.FloatField()
     precio3 = models.FloatField()
     precio4 = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
     
+    
+
 class Factura(models.Model):
     cliente = models.CharField(max_length=500)
     numero_factura = models.CharField(max_length=255)
