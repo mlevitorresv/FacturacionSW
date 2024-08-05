4e60870ac56ecedd5401198009ce35f3083ad4f5
mlevitorresv <levitorres66@gmail.com>
2024-08-05 09:32:27 +0200

    .exe creado correctamente

--- FacturacionSW/app.py
+++ FacturacionSW/app.py
@@ -1,13 +1,19 @@
 import webview
 import subprocess
 import time
+import os
+from pathlib import Path
+
+# Define el directorio base del proyecto
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
 
 # Inicia el servidor de desarrollo de Django
-subprocess.Popen(["python", "manage.py", "runserver"])
+subprocess.Popen(["python", BASE_DIR / "manage.py", "runserver"])
 
 # Espera a que el servidor esté en funcionamiento
 time.sleep(5)
 
 # Crea una ventana con PyWebview y carga tu aplicación Django
-webview.create_window('Mi App Django', 'http://localhost:8000')
+webview.create_window('CoronaCastilla', 'http://localhost:8000')
 webview.start()
