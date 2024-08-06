76d96afbadc5a7105f90384275ce3819dbedebf7
mlevitorresv <levitorres66@gmail.com>
2024-08-06 10:54:13 +0200

    add .bat for run app

---
+++ CoronaCastilla.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Cambia al directorio del proyecto
+cd .\FacturacionSW\
+
+REM Ejecuta el servidor Django en segundo plano
+start "" python manage.py runserver
+
+REM Espera de 5 segundos para asegurar que el servidor está en funcionamiento
+timeout /t 5 /nobreak >nul
+
+REM Abre el navegador en la dirección del poryecto django
+start http://localhost:8000
