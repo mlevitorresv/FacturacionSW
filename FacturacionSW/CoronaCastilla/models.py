411e2d52e20b701b3f9e79c8fd3052e4db8e759b
mlevitorresv <levitorres66@gmail.com>
2024-08-19 10:27:48 +0200

    RM: articulo modelo y modificado factura modelo

--- FacturacionSW/CoronaCastilla/models.py
+++ FacturacionSW/CoronaCastilla/models.py
@@ -1,26 +1,9 @@
 from django.db import models
-class Articulo(models.Model):
-    CATEGORIAS = [
-        ('alojamiento', 'Alojamiento'),
-        ('desayuno', 'Desayuno'),
-    ]
-
-    nombre = models.CharField(max_length=255)
-    categoria = models.CharField(max_length=20, choices=CATEGORIAS)
-    precio1 = models.FloatField()
-    precio2 = models.FloatField()
-    precio3 = models.FloatField()
-    precio4 = models.FloatField()
-
-    def __str__(self):
-        return self.nombre
-    
-    
 
 class Factura(models.Model):
     cliente = models.CharField(max_length=500)
     numero_factura = models.CharField(max_length=255)
-    habitacion = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    habitacion = models.CharField(max_length=255)
     habitacion_numero = models.IntegerField()
     fecha_entrada = models.DateField()
     fecha_salida = models.DateField()
