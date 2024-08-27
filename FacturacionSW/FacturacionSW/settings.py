c6372f8ee2c698c42f3edaf4c8e92b45cf137225
mlevitorresv <levitorres66@gmail.com>
2024-08-27 10:10:03 +0200

    add .env a /FacturacionSW

--- FacturacionSW/FacturacionSW/settings.py
+++ FacturacionSW/FacturacionSW/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-env_path = '.env'
-env.read_env(env_path)
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
