766ab0f9e9a590d6536b1f7797f66f1a265a7c8e
mlevitorresv <levitorres66@gmail.com>
2024-09-27 10:13:26 +0200

    formulario de factura modificado sin habitacio0nes

--- FacturacionSW/CoronaCastilla/forms.py
+++ FacturacionSW/CoronaCastilla/forms.py
@@ -2,23 +2,20 @@ from django import forms
 from CoronaCastilla.models import Factura, Cliente
 from django.shortcuts import get_object_or_404
 
-class facturaForm(forms.ModelForm):
+class FacturaForm(forms.ModelForm):
     class Meta:
         model = Factura
         fields = [
-            'cliente', 'numero_factura', 'habitacion_numero', 'fecha_entrada', 'fecha_salida',
-            'alojamiento_dias', 'desayuno_dias', 'alojamiento_precio', 'desayuno_precio', 
-            'base_imponible', 'porcentaje_iva', 'importe_iva', 'total_factura', 'habitacion', 'numero_cuenta'
+            'cliente', 'numero_factura', 'fecha_entrada', 'fecha_salida',
+            'desayuno_dias', 'desayuno_precio', 
+            'base_imponible', 'porcentaje_iva', 'importe_iva', 'total_factura', 'numero_cuenta'
         ]
         widgets = {
             'cliente': forms.Textarea(attrs={'class': 'plantilla__form__basico__area', 'rows': '8', 'id': 'clienteDetails'}),
             'numero_factura': forms.TextInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'numero'}),
-            'habitacion_numero': forms.NumberInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'numeroHabitacion'}),
             'fecha_entrada': forms.DateInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'fechaEntrada', 'type': 'date'}),
             'fecha_salida': forms.DateInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'fechaSalida', 'type': 'date'}),
-            'alojamiento_dias': forms.NumberInput(attrs={'class': 'plantilla__form__tabla__tbody__trow__tdata__input', 'name': 'numeroDias', 'id': 'alojamientoDias', 'onchange': 'precioAlojamiento()'}),
             'desayuno_dias': forms.NumberInput(attrs={'class': 'plantilla__form__tabla__tbody__trow__tdata__input', 'name': 'numeroDias', 'id': 'desayunoDias', 'onchange': 'precioDesayuno()'}),
-            'alojamiento_precio': forms.NumberInput(attrs={'class': 'plantilla__form__tabla__tbody__trow__tdata__input', 'name': 'precioAlojamiento', 'id': 'alojamientoPrecio', 'onchange': 'precioAlojamiento()'}),
             'desayuno_precio': forms.NumberInput(attrs={'class': 'plantilla__form__tabla__tbody__trow__tdata__input', 'name': 'precioDesayuno', 'id': 'desayunoPrecio', 'onchange': 'precioDesayuno()'}),
             'base_imponible': forms.NumberInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'baseImponible', 'id': 'baseImponible'}),
             'porcentaje_iva': forms.NumberInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'porcentajeIva', 'value': 10, 'id': 'porcentajeIva', 'onchange': 'showResults()'}),
@@ -26,16 +23,6 @@ class facturaForm(forms.ModelForm):
             'total_factura': forms.NumberInput(attrs={'class': 'plantilla__form__articulos__inputs__input', 'name': 'totalFactura', 'id': 'totalFactura'}),
             'numero_cuenta': forms.Textarea(attrs={'class': 'plantilla__form__basico__area', 'rows': '1', 'id': 'numero_cuenta', 'required': 'False'}),
         }
-
-    habitacion = forms.ChoiceField(
-        choices=[
-            ('habitación individual', 'Habitación individual'),
-            ('habitación doble', 'Habitación doble'),
-            ('habitación triple', 'Habitación triple'),
-            ('habitación cuadruple', 'Habitación cuadruple')
-        ],
-        widget=forms.Select(attrs={'class': 'plantilla__form__articulos__inputs__input', 'id': 'tipoHabitacion'})
-    )
 
 class clienteForm(forms.ModelForm):
     class Meta:
