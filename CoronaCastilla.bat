711f7865e5e3726c0b0147cd6b9c8ac11283722a
mlevitorresv <levitorres66@gmail.com>
2024-10-29 12:39:40 +0100

    cambios para iniciar app desde .bat

--- CoronaCastilla.bat
+++ CoronaCastilla.bat
@@ -2,11 +2,5 @@
 REM Cambia al directorio del proyecto
 cd .\FacturacionSW\
 
-REM Ejecuta el servidor Django en segundo plano
-start "" python manage.py runserver
-
-REM Espera de 5 segundos para asegurar que el servidor está en funcionamiento
-timeout /t 5 /nobreak >nul
-
 REM Abre el navegador en la dirección del poryecto django
 start python run_app.py
