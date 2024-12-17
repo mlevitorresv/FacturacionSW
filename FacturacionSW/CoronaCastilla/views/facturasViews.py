4a7529cab86347695eaed53f96889a18f4bcd94c
mlevitorresv <levitorres66@gmail.com>
2024-12-17 09:50:10 +0100

    orden de las facturas por numero_factura

--- FacturacionSW/CoronaCastilla/views/facturasViews.py
+++ FacturacionSW/CoronaCastilla/views/facturasViews.py
@@ -145,7 +145,7 @@ def view_facturas(request):
         for mes in meses_seleccionados:
             query |= Q(fecha_salida__year=año, fecha_salida__month=mes)
 
-        facturas = Factura.objects.filter(query).order_by('-id')
+        facturas = Factura.objects.filter(query).order_by('-numero_factura')
 
         # Filtrar por nombre de cliente si está en la URL
         cliente_nombre = request.GET.get('cliente', '')
